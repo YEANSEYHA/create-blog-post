2b35f16c7f8577e510921ae3d09d5b832cfe225a
Yean Seyha <yeanseyha7@gmail.com>
2022-03-09 15:17:54 +0700

    Update Login.vue page

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,14 @@
 <template>
   <header class="title bg-orange-400 flex justify-between h-12 align-middle">
-      
         <div class="m-4">
           <nuxt-link class=" text-primary" to="/">Home</nuxt-link>
           <nuxt-link class=" text-primary ml-2" to="/about">About</nuxt-link>
           <nuxt-link class=" text-primary ml-2" to="/admin/dashboard">Dashboard</nuxt-link>
+          <nuxt-link class=" text-primary ml-2" to="/auth/login">Login</nuxt-link>
         </div>
-
         <div class="m-4">
           <AppSearchInput />
         </div>
-        
   </header>
 </template>
 
