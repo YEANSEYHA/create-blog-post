9899ba81de1e281cca0914cd4c6528b31f8c4e7d
Yean Seyha <yeanseyha7@gmail.com>
2022-03-11 15:36:50 +0700

    Update login.vue

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -2,7 +2,7 @@
 <!-- component -->
 <div class="flex items-center justify-center">
         <div class="w-full max-w-md">
-          <form class="bg-white shadow-lg rounded py-12 px-12 pt-6 pb-8 mb-4">
+          <form @submit.prevent="authUser" class="bg-white shadow-lg rounded py-12 px-12 pt-6 pb-8 mb-4">
             <!-- @csrf -->
             <div
               class="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4"
@@ -19,6 +19,7 @@
               <input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 name="email"
+                v-model="inputEmail"
                 type="email"
                 required
                 autofocus
@@ -35,6 +36,7 @@
               <input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 type="password"
+                v-model="inputPassword"
                 placeholder="Password"
                 name="password"
                 required
@@ -56,8 +58,58 @@
 </template>
 
 <script>
-export default {
 
+export default {
+  created(){
+    console.log(this.users.length)
+    console.log(this.users[1].password)
+  },
+  data(){
+    return{
+      users : [
+      {
+        'name': 'admin',
+        'email': 'admin@gmail.com',
+        'password': 123456,
+        'role': 'admin'
+      },
+      {
+        'name': 'dara',
+        'email': 'dara@gmail.com',
+        'password': 123456,
+        'role': 'user'
+      }
+      ],
+      inputName:'',
+      inputEmail: '',
+      inputPassword:'',
+      inputRole:''
+    }
+  },
+  methods:{
+    authUser(){
+      for(let i=0;i<this.users.length;i++){
+        if(this.users[i].email == this.inputEmail && this.users[i].password == this.inputPassword ){
+          alert('Access Granted')
+          this.$router.push('/admin/dashboard')
+          break;
+        }
+        else{
+          alert('Access Denied')
+          break;
+        }
+      }
+    }
+    /* authUser(){
+      if(this.inputEmail === this.users[1].email && this.inputPassword == this.users[1].password){
+        alert('Access Granted')
+        this.$router.push('/admin/dashboard')
+      }
+      else{
+        alert('Access Denied')
+      }
+    } */
+  }
 }
 </script>
 
